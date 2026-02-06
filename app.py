1afdc9689b2d1ead1e8189799168a5235f36e834
daraalmaqam-lab <daraalmaqam@gmail.com>
2026-02-07 01:01:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ if "auth" not in st.session_state:
 
 if not st.session_state.auth:
     # العنوان
-    st.markdown('<div class="main-title-center">طيران المسار الذهبي</div>', unsafe_allow_html=True)
+    st.markdown('<div class="main-title-center">تــاشــيرات</div>', unsafe_allow_html=True)
 
     # حقل اسم المستخدم
     st.markdown('<div class="input-row"><div class="label-style">اسم المستخدم</div>', unsafe_allow_html=True)
@@ -145,3 +145,4 @@ else:
     if st.button("خروج"):
         st.session_state.auth = False
         st.rerun()
+
